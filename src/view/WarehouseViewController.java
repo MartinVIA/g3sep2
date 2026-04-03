d7bce780a1302c8e72da275e24e522d486db71b6
HNande <78953559+HNande@users.noreply.github.com>
2026-04-03 16:36:51 +0200

    View Products 1/2

--- src/view/WarehouseViewController.java
+++ src/view/WarehouseViewController.java
@@ -1,9 +1,22 @@
 package view;
 
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
+import model.Product;
 import viewmodel.WarehouseViewModel;
 
 public class WarehouseViewController {
+  @FXML private TableView<Product> productTable;
+  @FXML private TableColumn<Product, String> name;
+  @FXML private TableColumn<Product, Integer> price;
+  @FXML private TableColumn<Product, Integer> quantity;
+  @FXML private TableColumn<Product, Boolean> perishableness;
   private ViewHandler viewHandler;
   private Region root;
   private WarehouseViewModel model;
@@ -11,6 +24,11 @@ public class WarehouseViewController {
     this.viewHandler = viewHandler;
     this.model = model;
     this.root = root;
+    name.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
+    price.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getPrice()));
+    quantity.setCellValueFactory(cell -> new SimpleIntegerProperty(cell.getValue().getQuantity()));
+    perishableness.setCellValueFactory(cell -> new SimpleBooleanProperty(cell.getValue().isPerishable()));
+    productTable.setItems(model.getModel().getWarehouseList());
   }
 
   public Region getRoot() {
