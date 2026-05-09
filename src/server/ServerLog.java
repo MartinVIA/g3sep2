144c6555c6c57854d697fd77687c626ca9623446
HNande <78953559+HNande@users.noreply.github.com>
2026-05-09 04:16:08 +0200

    Server implemented

--- src/server/ServerLog.java
+++ src/server/ServerLog.java
@@ -1,5 +1,29 @@
 package server;
 
+import java.io.FileNotFoundException;
+import java.time.LocalTime;
+
 public class ServerLog {
 
+//    if a filepath is not specified it will automatically go to utils/server_log.txt
+    // newLog clears the file, starting from 0 at StartServer begining
+
+    public void log(String filepath, String text)throws FileNotFoundException{
+        utils.MyFileHandler.appendToTextFile(filepath,text+" "+now());
+    }
+    public void log(String text)throws FileNotFoundException{
+        utils.MyFileHandler.appendToTextFile("src/client/utils/server_log.txt",text+" "+now());
+    }
+
+    public void newLog(String filepath, String text)throws FileNotFoundException{
+        utils.MyFileHandler.writeToTextFile(filepath,text+" "+now());
+    }
+    public void newLog(String text)throws FileNotFoundException{
+        utils.MyFileHandler.writeToTextFile("src/client/utils/server_log.txt",text+" "+now());
+    }
+
+    private String now(){
+        LocalTime time= LocalTime.now();
+        return time.toString();
+    }
 }
--- src/server/StartServer.java
+++ src/server/StartServer.java
@@ -11,11 +11,13 @@ import java.util.ArrayList;
 public class StartServer {
   public static void main(String[] args) throws IOException {
     final int PORT = 4026;
+    ServerLog logger=new ServerLog();
 
     ServerSocket serverSocket = new ServerSocket(PORT);
     System.out.println("Waiting for connection on " + serverSocket.getLocalPort());
+    logger.newLog("Waiting for connection on " + serverSocket.getLocalPort());
 
-    Gson gson = new Gson();
+      Gson gson = new Gson();
     ArrayList<ClientHandler> handlerList = new ArrayList<>();
     ArrayList<Product> masterList = new ArrayList<>();
     Socket socket;
@@ -26,7 +28,8 @@ public class StartServer {
       Thread thread = new Thread(handler);
       thread.setDaemon(true);
       thread.start();
-      System.out.println("Connection Established");
+      System.out.println("Connection Established "+socket.getRemoteSocketAddress());
+      logger.log("Connection Established "+socket.getRemoteSocketAddress());
     }
   }
 }
