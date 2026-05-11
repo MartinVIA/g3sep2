85458a7ce46e8ff7edd2cdbb87caa95af9e792c0
Martin <GMChz@via.dk>
2026-05-11 11:02:05 +0200

    server logger working with real time

--- src/server/StartServer.java
+++ src/server/StartServer.java
@@ -11,11 +11,13 @@ import java.util.ArrayList;
 public class StartServer {
   public static void main(String[] args) throws IOException {
     final int PORT = 4026;
+    ServerLog logger=new ServerLog();
 
     ServerSocket serverSocket = new ServerSocket(PORT);
     System.out.println("Waiting for connection on " + serverSocket.getLocalPort());
+    logger.newLog("Waiting for connection on " + serverSocket.getLocalPort());
 
-    Gson gson = new Gson();
+      Gson gson = new Gson();
     ArrayList<ClientHandler> handlerList = new ArrayList<>();
     ArrayList<Product> masterList = new ArrayList<>();
     Socket socket;
@@ -27,6 +29,7 @@ public class StartServer {
       thread.setDaemon(true);
       thread.start();
       System.out.println("Connection Established "+socket.getRemoteSocketAddress());
+      logger.log("Connection Established "+socket.getRemoteSocketAddress());
     }
   }
 }
