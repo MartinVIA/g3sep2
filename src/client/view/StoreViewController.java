cb15ae9bce9e96220d28def9c7eee56ee0f08ec0
Martin <GMChz@via.dk>
2026-05-16 20:45:51 +0200

    discarding stock for stores is set

--- src/client/view/StoreViewController.java
+++ src/client/view/StoreViewController.java
@@ -4,6 +4,7 @@ import client.mediator.Client;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -12,9 +13,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
 import client.model.Product;
 import client.viewmodel.StoreViewModel;
+import javafx.scene.text.Text;
 
 public class StoreViewController {
-  @FXML private TableView<Product> productTable;
+    @FXML private TableView<Product> productTable;
   @FXML private TableColumn<Product, String> name;
   @FXML private TableColumn<Product, String> description;
   @FXML private TableColumn<Product, Integer> price;
@@ -22,7 +24,9 @@ public class StoreViewController {
   @FXML private TableColumn<Product, Boolean> perishableness;
   @FXML private Button warehouseButton;
   @FXML private Button backButton;
+  @FXML private Button discardButton;
   @FXML private ComboBox<String> storeSelector;
+  @FXML private Text nonSelectedWarning;
   private ViewHandler viewHandler;
   private Region root;
   private StoreViewModel model;
@@ -68,6 +72,21 @@ public class StoreViewController {
 
   public void handleBackButton(){
     viewHandler.openView("mainView");
+  }
+
+    public void handleDiscardButton() {
+      Product selected=productTable.getSelectionModel().getSelectedItem();
+      if (selected==null){
+          nonSelectedWarning.visibleProperty().set(true);
+      }
+      if (selected==null)
+          return;
+        selected.setQuantity(0);
+        productTable.refresh();
+        nonSelectedWarning.visibleProperty().set(false);
+
+
+
   }
 
 }
